da15873e0997f514d68179b53aa44703dfcb793b
jaxknox <jaxonknox321@gmail.com>
2022-01-14 20:54:31 -0600

    Set up Player and Computer class.

--- RockPaperScissors/src/com/company/Player.java
+++ RockPaperScissors/src/com/company/Player.java
@@ -1,4 +1,24 @@
 package com.company;
+import java.util.Scanner;
 
 public class Player {
+
+    private String playerMove;
+
+    //constructor
+    public Player(){
+
+    }
+
+    public void getPlayerInput(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please select your move: (R)ock, (P)aper, (S)cissors");
+        playerMove = scanner.nextLine().toUpperCase();
+
+    }
+
+    public String getPlayerMove(){
+        return playerMove;
+    }
 }
