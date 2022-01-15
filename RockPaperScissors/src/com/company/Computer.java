4f08ed9c83a4a6f1cef5e9156ebca923c011629b
jaxknox <jaxonknox321@gmail.com>
2022-01-14 22:29:38 -0600

    Classes filled out and ready to pull into master on git.

--- RockPaperScissors/src/com/company/Computer.java
+++ RockPaperScissors/src/com/company/Computer.java
@@ -7,7 +7,7 @@ public class Computer {
     //constructor
     public Computer(){
         String[] moves = {"Rock", "Paper", "Scissors"};
-        computerMove = "Rock";
+        computerMove = moves[new Random().nextInt(moves.length)];
     }
 
 
